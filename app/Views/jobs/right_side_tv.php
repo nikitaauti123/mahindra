480c5cc49d3278422421949438cc34e95de203b7
Shriram <shriramw@quicsolv.in>
2023-11-13 15:58:21 +0530

    changes done for TV screens

--- app/Views/jobs/right_side_tv.php
+++ app/Views/jobs/right_side_tv.php
@@ -1,26 +1,9 @@
 
-<?= $this->extend('theme-default') ?>
+<?= $this->extend('theme-tv') ?>
 
 <?= $this->section('content') ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0"><?php echo lang('Jobs.Add'); ?></h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#"><?php echo lang('Left-sidebar.Menu.Home'); ?></a></li>
-                        <li class="breadcrumb-item active"><?php echo lang('Jobs.Add'); ?></li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
 
     <!-- Main content -->
     <section class="content">
@@ -29,86 +12,6 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                        <div class="card-header">
-                            <?php
-
-                            $hide_start = '';
-                            $hide_stop = 'display:none';
-                            $hide_details = 'display:none';
-
-                            if (is_array($jobs) && count($jobs) > 0) {
-                                $hide_start = 'display:none';
-                                $hide_stop = '';
-                                $hide_details = '';
-                            }
-                            ?>
-                            <div class="row d-flex align-items-center justify-content-center">
-                                <div class="col-3 text-center">
-                                    <div class="digital-clock" style="display: none;">00:00:00</div>
-                                    <div class="row d-flex align-items-center justify-content-center">
-                                        <div class="col-12">
-                                            <div class="d-flex align-items-center justify-content-center">
-                                                <div class="m-1" id="start_time_right_parent" style="<?php echo $hide_start; ?>">
-                                                    <select class="form-control" id="part_right_id" name="part_right_id" class="from-control">
-                                                        <option value="">Select <?php echo lang('Jobs.PartName'); ?></option>
-                                                        <?php
-                                                        if (!empty($parts)) {
-                                                            foreach ($parts as $part) {
-                                                                echo '<option value="' . $part['id'] . '">' . $part['part_name'] . '</option>';
-                                                            }
-                                                        }
-                                                        ?>
-                                                    </select>
-                                                </div>
-                                                <input type="hidden" id="update_id_right" name="update_id_right" value="<?php if(is_array($jobs) && count($jobs)>0) { echo $jobs[0]['part_id']; }?>" >
-                                                <div class="m-1">
-                                                    <a href="javascript:void(0)" class="btn btn-success start_time_right" id="start_time" style="<?php echo $hide_start; ?>">
-                                                        Start
-                                                    </a>
-                                                </div>
-                                                <div class="m-1">
-                                                    <a href="javascript:void(0)" class="btn btn-danger end_time_right" id="stop_time" style="<?php echo $hide_stop; ?>">
-                                                        Stop
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-7">
-                                    <div class="row" id="display_part-details" style="<?php echo  $hide_details; ?>">
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <label for="part_name"><?php echo lang('Jobs.PartName'); ?></label> :
-                                                <span class="part_name"></span>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <label for="part_name"><?php echo lang('Jobs.PartNo'); ?></label> :
-                                                <span id="part_no"></span>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <label for="part_name"><?php echo lang('Jobs.Model'); ?></label> :
-                                                <span id="model"></span>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <div class="form-group">
-                                                <label for="part_name"><?php echo lang('Jobs.DieNo'); ?></label> :
-                                                <span id="die_no"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-2 text-right">
-                                    <a href="<?php echo base_url('/admin/jobs/list'); ?>" class="btn btn-primary"><?php echo lang('Jobs.JobsList'); ?></a>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- /.card-header -->
                         <div class="card-body no-pad">
                             <div class="row">
                                 <div class="col-md-12">
--- app/Views/nav-header-tv.php
+++ app/Views/nav-header-tv.php
@@ -6,7 +6,13 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="<?php echo base_url('admin/dashboard'); ?>" class="nav-link"><i class="fa fa-dashboard"></i></a>
+            <a href="<?php echo base_url('admin/dashboard'); ?>" class="nav-link">Dashboard</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="<?php echo base_url('admin/left_side_tv'); ?>" class="nav-link">Left TV</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="<?php echo base_url('admin/right_side_tv'); ?>" class="nav-link">Right TV</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <?php if(session()->get('isLoggedIn')) { ?>
@@ -17,28 +23,24 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
-        <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-                <form class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
+        <li>
+            <div class="row" style="width:650px;">
+                <div class="col-md-8">
+                    <label for="">Part Name: </label>&nbsp;<span class="part_name"> LEFT SIDE DOOR FOR CAR </span>
+                </div>
+                <div class="col-md-4">
+                    <label for="">Die No: </label>&nbsp;<span class="die_no"> 99 </span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <label for="">Model: </label>&nbsp;<span class="model"> XUV 700 </span>
+                </div>
+                <div class="col-md-4">
+                    <label for="">Part No: </label>&nbsp;<span class="part_no"> PT92343 </span>
+                </div>
             </div>
         </li>
-
         <!-- Messages Dropdown Menu -->
         <li class="nav-item dropdown">
            <!--  <a class="nav-link" data-toggle="dropdown" href="#">
